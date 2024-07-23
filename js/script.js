ab8e6ddead5fa67985492ee922b30d91fa4c1575
Iuri Soares <yurirodrigo17@outlook.com>
2024-07-23 11:44:10 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper-container", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  // Função para mudar os slides automaticamente
+  function autoSlide() {
+    swiper.slideNext();
+  }
+
+  // Define o intervalo para mudar os slides a cada 3 segundos (3000 ms)
+  setInterval(autoSlide, 500000);
+});
